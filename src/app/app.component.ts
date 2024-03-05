ff09bdf4e0196118261994808fa7a8492b3c1a7d
Gilvan-Saraiva <juniorsaraivabi@gmail.com>
2024-03-05 18:16:24 -0300

    headers and homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeadersComponent } from './headers/headers.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './principal/inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeadersComponent, RodapeComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projeto';
+  title = 'Loja';
 }
